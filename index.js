f4745c55c798799ae948a1895931b93d6b8a7ae0
Ian Raburu <williamraburu@gmail.com>
2023-06-14 06:59:29 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply); 
+
+// let random;
+let random = Math.ceil(Math.random());
+
+let num3 = 16;
+let num4 = 6;
+let mod =  num3 % num4;
+console.log(mod);
+
+let max = Math.max(10, 5, 20, 15);
+console.log(max);
